0ea77f1392624c970541ddd7a54567e0abdc8ad1
edavis25 <davis.25811@gmail.com>
2017-07-30 23:18:08 -0400

    Removed empty import line in Alert Subscriptions Controller.

--- app/Http/Controllers/AlertSubscriptionsController.php
+++ app/Http/Controllers/AlertSubscriptionsController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers;
 //use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
-
 use Request;
 
 class AlertSubscriptionsController extends Controller
